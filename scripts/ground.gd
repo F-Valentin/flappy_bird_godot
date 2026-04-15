64b3927317130774069243f3ae5d4ec5841aa53c
val <val@vals-MacBook-Air.local>
2026-04-15 23:57:31 +0200

    first push flappy_bird

---
+++ scripts/ground.gd
@@ -0,0 +1,5 @@
+extends StaticBody2D
+
+
+func _on_death_zone_body_entered(body: Node2D) -> void:
+	print("j")
---
+++ scripts/player.gd
@@ -0,0 +1,38 @@
+extends RigidBody2D
+
+var started: bool = false
+
+var flap_force: float = -340.0
+var flap_angular_force: float = -8.0
+
+var maximum_rotation_up: float = -30.0
+var maximum_rotation_down: float = 90.0
+var falling_angular_velocity: float = 5.0
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer 
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("Flap"):
+		if not started:
+			start_game()
+		flap()	
+	
+	if rotation_degrees <= maximum_rotation_up:
+		rotation_degrees = maximum_rotation_up
+		angular_velocity = 0.0
+
+	if linear_velocity.y > 0.0:
+		if rotation_degrees <= maximum_rotation_down:
+			angular_velocity = falling_angular_velocity
+		else:
+			angular_velocity = 0.0
+
+func start_game() -> void:
+	started = true
+	gravity_scale = 1.0
+
+
+func flap() -> void:
+	linear_velocity.y = flap_force
+	angular_velocity = flap_angular_force
+	animation_player.play()
